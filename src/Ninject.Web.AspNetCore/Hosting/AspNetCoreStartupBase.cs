6b0286e68b618d05bdd6d699f6c280863568d8f4
Lukas Angerer <executor.dev@gmail.com>
2020-11-04 19:07:15 +0100

    Updated Startup base class

--- src/Ninject.Web.AspNetCore/Hosting/AspNetCoreStartupBase.cs
+++ src/Ninject.Web.AspNetCore/Hosting/AspNetCoreStartupBase.cs
@@ -23,9 +23,8 @@ namespace Ninject.Web.AspNetCore.Hosting
 		IServiceProvider IStartup.ConfigureServices(IServiceCollection services)
 		{
 			var mvcBuilder = services.AddMvc()
-				.SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+				.SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 			ConfigureMvcOptions(mvcBuilder);
-			services.AddRouting();
 			ConfigureServices(services); // allow to customize the services before conversion to NInject bindings happen.
 			return _providerFactory.CreateBuilder(services).Build();
 		}
